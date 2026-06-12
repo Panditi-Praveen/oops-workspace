7e967ca925f78c73401f4c562295daafb9bd5307
Panditi-Praveen <panditipraveen467@gmail.com>
2026-06-12 21:23:58 +0530

    feat:collections added

---
+++ oops-workspace/src/com/java/collections/Product.java
@@ -0,0 +1,54 @@
+package com.java.collections;
+
+import java.util.Objects;
+
+public class Product {int id;
+    String name;
+    int maxRetailPrice;
+    float rating;
+    int reviewsCount;
+    byte discountPercentage;
+    boolean isAvailable;
+    String company;
+    String category;
+
+    public Product(int id, String name, int maxRetailPrice, float rating, int reviewsCount, byte discountPercentage, boolean isAvailable, String company, String category) {
+        this.id = id;
+        this.name = name;
+        this.maxRetailPrice = maxRetailPrice;
+        this.rating = rating;
+        this.reviewsCount = reviewsCount;
+        this.discountPercentage = discountPercentage;
+        this.isAvailable = isAvailable;
+        this.company = company;
+        this.category = category;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && maxRetailPrice == product.maxRetailPrice && Float.compare(rating, product.rating) == 0 && reviewsCount == product.reviewsCount && discountPercentage == product.discountPercentage && isAvailable == product.isAvailable && Objects.equals(name, product.name) && Objects.equals(company, product.company) && Objects.equals(category, product.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, maxRetailPrice, rating, reviewsCount, discountPercentage, isAvailable, company, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", maxRetailPrice=" + maxRetailPrice +
+                ", rating=" + rating +
+                ", reviewsCount=" + reviewsCount +
+                ", discountPercentage=" + discountPercentage +
+                ", isAvailable=" + isAvailable +
+                ", company='" + company + '\'' +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
